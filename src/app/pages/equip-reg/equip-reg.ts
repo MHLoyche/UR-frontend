2a93afa5b681965005514e6911720fcbeaf21b09
MHLoyche <MHLoyche@outlook.com>
2026-06-18 10:37:51 +0200

    pages

---
+++ src/app/pages/equip-reg/equip-reg.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equip-reg',
+  imports: [],
+  templateUrl: './equip-reg.html',
+  styleUrl: './equip-reg.css',
+})
+export class EquipReg {}
