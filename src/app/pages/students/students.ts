c40c495a6ed51c952ab30e2a9d6c3d903b65ea90
MHLoyche <MHLoyche@outlook.com>
2026-06-23 09:59:14 +0200

    student table

--- src/app/pages/students/students.ts
+++ src/app/pages/students/students.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { map } from 'rxjs';
+
+export interface StudentJson {
+  elevnummer: string;
+  navn: string;
+  adresse: string;
+  postnummerOgBy: string;
+  cprNummer: string;
+  email: string;
+  stamklasse: string;
+}
+
+type StudentsResponse = {
+  users: StudentJson[];
+};
 
 @Component({
   selector: 'app-students',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './students.html',
   styleUrl: './students.css',
 })
-export class Students {}
+export class Students {
+  private readonly http = inject(HttpClient);
+
+  readonly students$ = this.http.get<StudentsResponse>('data/mock-data/users.json').pipe(
+    map(response => response.users)
+  );
+}
